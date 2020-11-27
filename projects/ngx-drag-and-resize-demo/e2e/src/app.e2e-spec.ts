9219fced536f2d8273a0f68f75d55d728083ba04
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-11-27 11:56:40 +0200

    feat: remove redundant files from demo app

--- projects/ngx-drag-and-resize-demo/e2e/src/app.e2e-spec.ts
+++ projects/ngx-drag-and-resize-demo/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,11 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {AppPage} from './app.po';
+import {browser, logging} from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
-  });
-
-  it('should display welcome message', async () => {
-    await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('ngx-drag-and-resize-demo app is running!');
   });
 
   afterEach(async () => {
