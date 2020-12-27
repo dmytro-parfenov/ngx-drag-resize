a3d13b90d408b1ab3568387afa562078d1046b1a
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 21:08:34 +0200

    feat(demo): add app title

--- projects/ngx-drag-resize-demo/src/app/app.module.ts
+++ projects/ngx-drag-resize-demo/src/app/app.module.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NgxDragResizeModule} from 'ngx-drag-resize';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import {NgxDragResizeModule} from 'ngx-drag-resize';
   ],
   imports: [
     BrowserModule,
-    NgxDragResizeModule
+    NgxDragResizeModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
