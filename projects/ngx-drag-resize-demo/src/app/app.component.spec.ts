b1d2110fa9c9ab35a4b28e8de841ee9da798025a
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2020-12-27 21:10:03 +0200

    test(demo): update AppComponent test

--- projects/ngx-drag-resize-demo/src/app/app.component.spec.ts
+++ projects/ngx-drag-resize-demo/src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {NgxDragResizeModule} from 'ngx-drag-resize';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -7,6 +9,10 @@ describe('AppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MatToolbarModule,
+        NgxDragResizeModule
+      ],
       declarations: [ AppComponent ]
     })
       .compileComponents();
